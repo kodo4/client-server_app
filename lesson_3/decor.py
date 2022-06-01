1a6bab6c67c5cd54b3ebc46ecd902637362441c5
Роман Кодочигов <kodochigovra@mail.ru>
2022-06-01 20:06:13 +0300

    fix decor

--- lesson_3/decor.py
+++ lesson_3/decor.py
@@ -1,6 +1,8 @@
+import inspect
 import logging
 import sys
 from log import server_log_config, client_log_config
+import traceback as tr
 
 
 def log(func):
@@ -11,8 +13,8 @@ def log(func):
         LOGGER = logging.getLogger(log_name)
 
         result = func(*args, **kwargs)
-        LOGGER.info(f'Функция {func.__name__}, аргументы: {args}, {kwargs}')
-        LOGGER.info(f'Функция {func.__name__} была вызвана из функции '
-                    f'{func.__module__}')
+        LOGGER.info(f'Функция "{func.__name__}", аргументы: {args}, {kwargs}')
+        LOGGER.info(f'Функция "{func.__name__}" была вызвана из функции '
+                    f'"{inspect.stack()[1][3]}"')
         return result
     return decorator
